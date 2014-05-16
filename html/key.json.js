6acddff37f8c6bdf18af39fef8c5a2fb34d5ca5d
Rafael Roque de Mello <roquebrasilia@gmail.com>
2014-05-16 12:17:03 -0300

    Publicado o último produto do trabalho colaborativo entre ICMBio,CMB e Telebras

---
+++ html/key.json.js
@@ -0,0 +1,8 @@
+PublishWebAttributes = {
+	useTheme: false,				// If you want personalize your export styles use theme builder for bizagi and copy the css file in /libs/css/theme.css
+	preserveHTML: true,			// If you want preserve your properties / description and attributes html format
+	showPublishDate: false,		// If you want to show your publish date
+	subNavigation: false,		// If you want to show a subnavigation bar for properties
+	showPerformersList: false,	// If you want to show a performers link
+	maxLenghtDescription: 1000	// Limits of characters in description (dialog)
+};
