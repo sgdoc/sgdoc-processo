6de55faec0213f64925e1fc8f3a9f26616a86084
Rafael Roque de Mello <roquebrasilia@gmail.com>
2014-05-16 10:02:33 -0300

    Publicação do mapeamento de processo no bizagi em html e xpdl 10/04/2014 com a equipe do ICMBIO-CMB-Telebras.

---
+++ html/libs/js/app/app.ie8detect.js
@@ -0,0 +1,19 @@
+
+
+function IsIE8Browser() {
+    var rv = -1;
+    var ua = navigator.userAgent;
+    var re = new RegExp("Trident\/([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) {
+        rv = parseFloat(RegExp.$1);
+    }
+    return (rv == 4);
+}
+
+if (IsIE8Browser()) {
+    var htmlObj = document.getElementsByTagName('html')[0];
+        htmlObj.className = htmlObj.className + ' no-js lt-ie9';
+
+    var element = document.getElementById('biz-ex-support');
+        element.innerHTML = Bizagi.AppModel.texts['unsupported'];
+}
